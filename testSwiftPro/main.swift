4088cf3f8230dc30aff6606fab0dc5ecf33ee1fa
perrychen901105 <chenzhipeng901105@gmail.com>
2014-06-23 18:11:24 +0800

    add properties function

--- testSwiftPro/main.swift
+++ testSwiftPro/main.swift
@@ -577,7 +577,7 @@ dictionaryB["A"] = 3
 
 println("\(dictionaryA)")
 
-
+// test properties
 struct FixedLengthRange {
     var firstValue: Int
     let length: Int
@@ -608,12 +608,58 @@ let manager = DataManager()
 manager.data += "Some data"
 manager.data += "Some more data"
 
-println(manager.importer.fileName)
+//println(manager.importer.fileName)
 
 
 
 
+struct Point {
+    var x = 0.0, y = 0.0
+}
 
+struct Size {
+    var width = 0.0, height = 0.0
+}
+
+struct Rect {
+    var origin = Point()
+    var size = Size()
+    var center: Point {
+    get {
+        let centerX = origin.x + (size.width / 2)
+        let centerY = origin.y + (size.height / 2)
+        return Point(x: centerX, y: centerY)
+    }
+    set(newCenter) {
+        origin.x = newCenter.x - (size.width / 2)
+        origin.y = newCenter.y - (size.height / 2)
+    }
+    }
+}
+
+var square = Rect(origin: Point(x: 0.0, y: 0.0), size: Size(width: 10.0, height: 10.0))
+let initialSquareCenter = square.center
+square.center = Point(x: 15.0, y: 15.0)
+println("square.origin is now at (\(square.origin.x), \(square.origin.y))")
+
+
+class StepCounter {
+    var totalSteps: Int = 0 {
+    willSet {
+        println("About to set totalSteps to \(newValue)")
+    }
+    didSet {
+        if totalSteps > oldValue {
+            println("Added \(totalSteps - oldValue) steps")
+        }
+    }
+    }
+}
+
+let stepCounter = StepCounter()
+stepCounter.totalSteps = 200
+stepCounter.totalSteps = 360
+stepCounter.totalSteps = 896
 
 
 
