9b663e81213b9eb2359f99c633fb239b927be814
perrychen901105 <chenzhipeng901105@gmail.com>
2014-06-24 18:12:48 +0800

    add class function

--- testSwiftPro/main.swift
+++ testSwiftPro/main.swift
@@ -506,7 +506,7 @@ println(c)
 
 */
 
-
+/*
 enum SomeEnumeration {
     
 }
@@ -662,10 +662,172 @@ stepCounter.totalSteps = 360
 stepCounter.totalSteps = 896
 
 
+struct SomeStructure {
+    static var storedTypeProperty = "Some value."
+    static var computedTypeProperty: Int {
+        get {
+            return 1
+    }
+        set {
+            
+    }
+    }
+}
+
+enum SomeEnumeration {
+    static var storedTypeProperty = "Some Value."
+    static var computedTypeProperty: Int {
+        get {
+            return 2
+    }
+    }
+}
+
+class SomeClass {
+    class var computedTypeProperty: Int {
+        get {
+            return 3
+    }
+    }
+}
+
+//println(SomeClass.computedTypeProperty)
+//println(SomeStructure.storedTypeProperty)
+//SomeStructure.storedTypeProperty = "Another value."
+//println(SomeStructure.storedTypeProperty)
+
+struct AudioChannel {
+    static let thresholdLevel = 10
+    static var maxInputLevelForAllChannels = 0
+    var currentLevel: Int = 0 {
+    didSet {
+        if currentLevel > AudioChannel.thresholdLevel {
+            currentLevel = AudioChannel.thresholdLevel
+        }
+        if currentLevel > AudioChannel.maxInputLevelForAllChannels {
+            AudioChannel.maxInputLevelForAllChannels = currentLevel
+        }
+    }
+    }
+}
+
+var leftChannel = AudioChannel()
+var rightChannel = AudioChannel()
+leftChannel.currentLevel = 7
+println(leftChannel.currentLevel)
+println(AudioChannel.maxInputLevelForAllChannels)
+println(rightChannel.currentLevel)
+
+*/
+
+// Methods
+class Counter {
+    var count: Int = 0
+    func increment() {
+        self.count++
+    }
+    func incrementBy(amount: Int) {
+        count += amount
+    }
+    func reset() {
+        count = 0
+    }
+    func incrementBy(#amount: Int, numberOfTimes: Int) {
+        count += amount * numberOfTimes
+    }
+}
+let counter = Counter()
+counter.increment()
+counter.incrementBy(5)
+counter.reset()
+
+counter.incrementBy(amount: 5, numberOfTimes: 3)
+
+struct Point {
+    var x = 0.0, y = 0.0
+    func isToTheRightOfX(x: Double) -> Bool {
+        return self.x > x
+    }
+    
+    mutating func moveByX(deltaX: Double, y deltaY: Double) {
+        x += deltaX
+        y += deltaY
+    }
+}
+var somePoint = Point(x: 4.0, y: 5.0)
+if somePoint.isToTheRightOfX(1.0) {
+    println("This point is to the right of the line where x == 1.0")
+}
+somePoint.moveByX(2.0, y: 3.0)
+
+enum TriStateSwitch {
+    case Off, Low, High
+    mutating func next() {
+        switch self {
+        case Off:
+            self = Low
+        case Low:
+            self = High
+        case High:
+            self = Off
+        }
+    }
+}
+var ovenLight = TriStateSwitch.Low
+ovenLight.next()
+ovenLight.next()
+
+class SomeClass {
+    class func someTypeMethod() {
+        
+    }
+}
+SomeClass.someTypeMethod()
+
+struct LevelTracker {
+    static var highestUnlockedLevel = 1
+    static func unlockLevel(level: Int) {
+        if level > highestUnlockedLevel {
+            highestUnlockedLevel = level
+        }
+    }
+    static func levelIsUnlocked(level: Int) -> Bool {
+        return level <= highestUnlockedLevel
+    }
+    var currentLevel = 1
+    mutating func advanceToLevel(level: Int ) -> Bool {
+        if LevelTracker.levelIsUnlocked(level) {
+            currentLevel = level
+            return true
+        } else {
+            return false
+        }
+    }
+}
+
+class Player {
+    var tracker = LevelTracker()
+    let playerName: String
+    func completedLevel(level: Int) {
+        LevelTracker.unlockLevel(level+1)
+        tracker.advanceToLevel(level+1)
+    }
+    init(name: String) {
+        playerName = name
+    }
+}
+
+var player = Player(name: "Argyrios")
+player.completedLevel(1)
+println("highest unlocked level is now \(LevelTracker.highestUnlockedLevel)")
 
 
-
-
+player = Player(name: "Beto")
+if player.tracker.advanceToLevel(6) {
+    
+} else {
+    
+}
 
 
 
