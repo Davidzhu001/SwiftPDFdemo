b94576102581fbc5caa476f539f3e1bc8ba74698
perrychen901105 <chenzhipeng901105@gmail.com>
2014-06-26 18:18:35 +0800

    add nest

--- testSwiftPro/main.swift
+++ testSwiftPro/main.swift
@@ -1472,9 +1472,44 @@ for thing in things {
 
 
 // Nested Types
-
-
-
+struct BlackjackCard {
+    // nested Suit enumeration
+    enum Suit: String {
+        case Spades = "♠︎", Hearts = "♡", Diamonds = "♢", Clubs = "♣︎"
+    }
+    
+    // nested Rank enumeration
+    enum Rank: Int {
+        case Tow = 2, Three, Four, Five, Six, Seven, Eight, Nine, Ten
+        case Jack, Queen, King, Ace
+        struct Values {
+            let first: Int, second: Int?
+        }
+        var values: Values {
+            switch self {
+            case .Ace:
+                return Values(first: 1, second: 11)
+            case .Jack, .Queen, .King:
+                return Values(first: 10, second: nil)
+            default:
+                return Values(first: self.toRaw(), second: nil)
+            }
+        }
+    }
+    let rank: Rank, suit: Suit
+    var description: String {
+    var output = "suit is \(suit.toRaw()),"
+        output += " value is \(rank.values.first)"
+        if let second = rank.values.second {
+            output += " or \(second)"
+        }
+        return output
+     }
+}
+let theAceOfSpades = BlackjackCard(rank: .Ace, suit: .Spades)
+println("theAceOfSpades: \(theAceOfSpades.description)")
+let heartsSymbol = BlackjackCard.Suit.Hearts.toRaw()
+println("the symbol is \(heartsSymbol)")
 
 
 
