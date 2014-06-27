528cc9419d98d70b330bda17f19216ea81a79cc8
perrychen901105 <chenzhipeng901105@gmail.com>
2014-06-27 10:33:52 +0800

    add extension codes

--- testSwiftPro/main.swift
+++ testSwiftPro/main.swift
@@ -282,7 +282,7 @@ println(description)
 */
 
 
-
+/*
 func sayHello(personName: String) -> String {
     let greeting = "Hello, " + personName + "!"
     return greeting
@@ -411,7 +411,7 @@ func printMathResult(mathFunction: (Int, Int) -> Int, a: Int, b: Int) {
 //func chooseStepFunction(backwards: Bool) -> (Int) -> Int {
 //    return backwards ? stepBackward : stepForward
 //}
-
+*/
 /*
 func chooseStepFunction(backwards: Bool) -> (Int) -> Int {
     func stepForward(input: Int) -> Int { return input + 1}
@@ -1470,7 +1470,7 @@ for thing in things {
 }
 */
 
-
+/*
 // Nested Types
 struct BlackjackCard {
     // nested Suit enumeration
@@ -1510,6 +1510,95 @@ let theAceOfSpades = BlackjackCard(rank: .Ace, suit: .Spades)
 println("theAceOfSpades: \(theAceOfSpades.description)")
 let heartsSymbol = BlackjackCard.Suit.Hearts.toRaw()
 println("the symbol is \(heartsSymbol)")
+*/
+
+/*
+// extension
+extension Double {
+    var km: Double { return self * 1_000.0 }
+    var m: Double { return self }
+    var cm: Double { return self / 100.0 }
+    var mm: Double { return self / 1_000.0 }
+    var ft: Double { return self / 3.28084 }
+}
+let aMarathon = 42.km + 195.m
+println("A marathon is \(aMarathon) meters long")
+
+struct Size {
+    var width = 0.0, height = 0.0
+}
+struct Point {
+    var x = 0.0, y = 0.0
+}
+struct Rect {
+    var origin = Point()
+    var size = Size()
+}
+let defaultRect = Rect()
+let memberwiseRect = Rect(origin: Point(x: 2.0, y: 2.0), size: Size(width: 5.0, height: 5.0))
+
+extension Rect {
+    init(center: Point, size: Size) {
+        let originX = center.x - (size.width / 2)
+        let originY = center.y - (size.height / 2)
+        self.init(origin: Point(x: originX, y: originY), size: size)
+    }
+}
+
+let centerRect = Rect(center: Point(x: 4.0, y: 4.0), size: Size(width: 3.0, height: 3.0))
+println("the center is \(centerRect)")
+
+extension Int {
+    func repetitions(task: () -> ()) {
+        for i in 0..self {
+            task()
+        }
+    }
+}
+
+3.repetitions({
+        println("Hello")
+    })
+
+extension Int {
+    mutating func square() {
+        self = self * self
+    }
+}
+var someInt = 3
+someInt.square()
+println("\(someInt)")
+
+extension Int {
+    subscript(digitIndex: Int) -> Int {
+        var decimalBase = 1
+            for _ in 1...digitIndex {
+                decimalBase *= 10
+            }
+            return (self / decimalBase) % 10
+    }
+}
+
+//extension Character {
+//    enum Kind {
+//        case Vowel, Consonant, Other
+//    }
+//    var Kind: Kind {
+//        switch String(self).lowercaseString {
+//            case "a", "e", "i", "o", "u":
+//                return .Vowel
+//            case "b", "c", "d", "f", "g", "h", "j", "k", "l", "m",
+//            "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z":
+//                return .Consonant
+//            default:
+//                return .Other
+//        }
+//    }
+//}
+*/
+
+
+
 
 
 
